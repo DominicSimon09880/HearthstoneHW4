8b0acf7ef80ec59d948862697a17cbd03fa5e041
Dom <dominic.simon@cuw.edu>
2021-02-16 01:46:02 -0600

    V2

--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,24 @@
-#include "Card.hpp"
+#include "card.hpp"
 
 int main(int argc, char** argv)
 {
     Card* KingKrush = new Card("King Krush", 8, 8, 8);
-    //(*KingKrush).display();
     KingKrush->display();
+  
+  
+Card* ImprisonedAntaen = new Card("Imprisoned Antaen", 6, 10, 6);
+    ImprisonedAntaen->display();
+
+Card* BladedLady = new Card("Bladed Lady", 6, 6, 6);
+    BladedLady->display();
+
+Card* EmeraldExplorer = new Card("Emerald Explorer", 6, 4, 8);
+    EmeraldExplorer->display();
+
+Card* ForestWardenOmu = new Card("Forest Warden Omu", 6, 5, 4);
+    ForestWardenOmu->display();
+
+
+
+
 }
